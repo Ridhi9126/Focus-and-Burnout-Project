549d0885c7679321bcc166441f01b8f9e1ac37cd
Ridhima Miglani <ridhimamiglani99@gmai.com>
2025-12-28 23:51:29 +0530

    evaluate model with predicton and accuracy

--- ml/train.model.py
+++ ml/train.model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score 
 
 df=pd.read_json("ml/dataset.json")
 
@@ -28,4 +29,11 @@ y = df['burnout_label']                                              # labels
 x_train, x_test, y_train, y_test= train_test_split(x,y,test_size=0.2,random_state=42)    #train-test-split
 model = LogisticRegression(max_iter=100)         # max_iter uses the maximum number of iteraions he model can use to converge during training
 model.fit(x_train,y_train)
-print("Model Trained Successfully")
+
+y_pred=model.predict(x_test)
+
+accuracy=accuracy_score(y_test,y_pred)
+print("accuracy:",accuracy)
+
+for actual, predicted in zip(y_test,y_pred):        # zip pairs each actual label with its corresponding predicted label so they can be compared together.
+    print(f"actual:{actual}, predicted:{predicted}") 
